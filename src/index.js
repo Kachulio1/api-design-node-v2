f2e08bff40ac309ef55ebf6fe1ed6ac632d55e4e
kachulio1 <jngugi88@gmail.com>
2018-04-11 17:14:14 +0300

    test if the app works

--- src/index.js
+++ src/index.js
@@ -12,3 +12,4 @@ if (module.hot) {
 		currentApp = app
 	})
 }
+
--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
-import express from 'express'
-import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
+import express from "express";
+import setupMiddware from "./middleware";
+import { restRouter } from "./api";
+import { connect } from "./db";
+import { signin, protect } from "./api/modules/auth";
 // Declare an app from express
-const app = null
+const app = express();
 
-export default app
+app.get("/", () => {
+  res.json({ ok: true });
+});
+
+app.listen(3001, () => {
+  console.log("http://localhost:3001");
+});
+export default app;
